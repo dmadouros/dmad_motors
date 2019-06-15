52b4a3b1637c9801d98103c8b628838769b0a8d8
David Madouros <david@earthvectors.com>
2019-06-14 22:30:05 -1000

    Move hard-coded list of cars to controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,3 +1,9 @@
 class CarsController < ApplicationController
-
+  def index
+    @cars = [
+      OpenStruct.new(make: "Toyota", model: "Camry", model_year: "2005"),
+      OpenStruct.new(make: "Toyota", model: "Corolla", model_year: "1999"),
+      OpenStruct.new(make: "Ford", model: "Explorer", model_year: "2005"),
+    ]
+  end
 end
